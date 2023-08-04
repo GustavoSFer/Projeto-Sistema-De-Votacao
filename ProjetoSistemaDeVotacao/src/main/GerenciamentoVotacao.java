ae785d7eeaf2dadfb28ce0add1c62a1a8aac4241
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-03 21:42:15 -0300

    Criando o método para cadastrar pessoa Candidata.

--- ProjetoSistemaDeVotacao/src/main/GerenciamentoVotacao.java
+++ ProjetoSistemaDeVotacao/src/main/GerenciamentoVotacao.java
@@ -3,6 +3,18 @@ package main;
 import java.util.ArrayList;
 
 public class GerenciamentoVotacao {
-  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<PessoaCandidata>();
   private ArrayList<PessoaEleitora> pessoaEleitora;
+  private ArrayList<String> cpfComputado;
+  private int totalVotos;
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoa : pessoasCandidatas) {
+      if (pessoa.getNumero() == numero) {
+        System.out.println("Número pessoa candidata já utilizado!");
+      } else {
+        pessoasCandidatas.add(new PessoaCandidata(nome, numero));
+      }
+    }
+  }
 }

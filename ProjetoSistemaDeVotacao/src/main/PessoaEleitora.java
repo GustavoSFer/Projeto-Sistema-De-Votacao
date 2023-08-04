ad39bf01d2c6f37a4d6951e98daaed8afe6c406a
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-03 21:19:10 -0300

    Criando a classe pessoaEleitora e seu metodo construtor.

---
+++ ProjetoSistemaDeVotacao/src/main/PessoaEleitora.java
@@ -0,0 +1,20 @@
+package main;
+
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  @Override
+  public String getNome() {
+    return super.nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    super.nome = nome;
+  }
+}

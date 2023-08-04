895ad4aa652901c42a5f5a87c36a89dfbcfa03e8
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-03 21:33:50 -0300

    Criando a classe para gerenciar os votos e os candidatos.

---
+++ ProjetoSistemaDeVotacao/src/main/GerenciamentoVotacao.java
@@ -0,0 +1,8 @@
+package main;
+
+import java.util.ArrayList;
+
+public class GerenciamentoVotacao {
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoaEleitora;
+}

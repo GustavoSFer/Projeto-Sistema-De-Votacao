6e10fa64acc32521882cebc08e6e9a64c77844cb
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-07 18:44:22 -0300

    Cadastrando a pessoa na classe que gerencia.

--- ProjetoSistemaDeVotacao/src/main/Principal.java
+++ ProjetoSistemaDeVotacao/src/main/Principal.java
@@ -7,29 +7,33 @@ public class Principal {
   public static void main(String[] args) {
     int opcao = 0;
     Scanner scanner = new Scanner(System.in);
+    GerenciamentoVotacao gerenciamento = new GerenciamentoVotacao();
 
     System.out.println("----------- Bem-vindo ao Sistema de Votação -----------");
 
     while (opcao != 2) {
       System.out.println("Cadastrar pessoa candidata?");
+      System.out.println("1- Sim\n2- Não\nEntre com o número correspondente à opção desejada:");
       opcao = scanner.nextInt();
 
       switch (opcao) {
         case 1:
-          cadastrarPessoaCandidata();
+          System.out.println("Entre com o nome da pessoa candidata:");
+          String nome = scanner.next();
+
+          System.out.println("Entre com o número da pessoa candidata:");
+          int numero = scanner.nextInt();
+
+          gerenciamento.cadastrarPessoaCandidata(nome, numero);
           break;
         case 2:
           break;
         default:
-          System.out.println("Opção invalida");
+          System.out.println("Opção invalida! Escolha uma opção valida: ");
       }
     }
 
-
-
-  }
-
-  public static void cadastrarPessoaCandidata() {
+    scanner.close();
 
   }
 }

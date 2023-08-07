d9ceab94dd4cce95da56635e29ad7d0bf3192a90
GustavoSFer <gustavo.sfernander@gmail.com>
2023-08-07 18:35:36 -0300

    Criando um laço de repetição para cadastrar pessoa candidata.

--- ProjetoSistemaDeVotacao/src/main/Principal.java
+++ ProjetoSistemaDeVotacao/src/main/Principal.java
@@ -1,10 +1,35 @@
 package main;
 
+import java.util.Scanner;
+
 public class Principal {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    int opcao = 0;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("----------- Bem-vindo ao Sistema de Votação -----------");
+
+    while (opcao != 2) {
+      System.out.println("Cadastrar pessoa candidata?");
+      opcao = scanner.nextInt();
+
+      switch (opcao) {
+        case 1:
+          cadastrarPessoaCandidata();
+          break;
+        case 2:
+          break;
+        default:
+          System.out.println("Opção invalida");
+      }
+    }
+
+
 
   }
 
+  public static void cadastrarPessoaCandidata() {
+
+  }
 }
